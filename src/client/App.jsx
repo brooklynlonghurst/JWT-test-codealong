55cebce20b700bf49fae18311f66b21bba1efcaf
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-02 10:07:12 -0600

    working on jwt token

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,16 +6,26 @@ import axios from 'axios'
 function App() {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [token, setToken] = useState("")
 
   const handleLogin = () => {
     axios
       .post('/api/login', {username, password})
       .then((res) => {
-        console.log(res)
+        setToken(res)
       })
       .catch((err) => {
         console.log(err)
       })
+  }
+
+  const handleTest = () => {
+    axios
+      .post('/api/test', {token})
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch((err))
   }
 
   return (
@@ -24,6 +34,7 @@ function App() {
       <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
       <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
       <button onClick={handleLogin}>Login</button>
+      <button onClick={handleTest}>Test</button>
     </div>
   );
 }
--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -1,5 +1,36 @@
+const jwt = require('jsonwebtoken')
+
+const createToken = (username) => {
+    return jwt.sign(
+        {
+            username
+        },
+        "donttellanyone", //this is typically a SECRET .env value
+        {
+            expiresIn: '2 minutes'
+        }
+    )
+}
+
 module.exports = {
     login: async (req, res) => {
-        res.status(200).send('Success')
+        const {username} = req.body
+        const token = createToken(username)
+        res.status(200).send(token)
+    }, 
+    test: async (req, res) => {
+        const {token} = req.body
+        
+        if(!token) {
+            return res.status(400).send("No token")
+        }
+
+        try {
+            let authenticToken = jwt.verify(token, "donttellanyone")
+            return res.status(200).send("Good token")
+        }
+        catch (err) {
+            return res.status(400).send("Bad token")
+        }
     }
 }
