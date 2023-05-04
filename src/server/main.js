c8117fe105534ffb418687a5fcb3e7acf54300de
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-04 16:06:32 -0600

    implements sequelize

--- src/server/main.js
+++ src/server/main.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const ViteExpress = require("vite-express");
 const {login, test} = require('./controllers/authController')
+const db = require('./util/database')
+const {User} = require('./util/models')
+
 
 const app = express();
 app.use(express.json())
@@ -11,6 +14,17 @@ app.get("/hello", (req, res) => {
 
 app.post('/api/login', login)
 app.post('api/test', test)
+app.post('/api/register', async (req, res) => {
+  try {
+    let newUser = await User.create(req.body)
+    res.status(200).send(newUser)
+  }
+  catch(err) {
+    res.status(400).send(err)
+  }
+})
+
+db.sync()
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
